bc11a61749f8173b176f3ee0143a309b99361ad4
kirkram <94983351+kirkram@users.noreply.github.com>
2024-02-09 17:41:47 +0200

    pipx works, refactor it

--- src/pipex_main.c
+++ src/pipex_main.c
@@ -6,7 +6,7 @@
 /*   By: klukiano <klukiano@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 11:24:04 by klukiano          #+#    #+#             */
-/*   Updated: 2024/02/07 17:21:44 by klukiano         ###   ########.fr       */
+/*   Updated: 2024/02/09 15:06:40 by klukiano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/wait.h>
+
+#include <errno.h>
 
 int		pipex(int *fd, char **av, char **envp);
 int		child_one(int *fd, char *arg_cmd_1, char **envp, int *end);
@@ -26,18 +29,34 @@ int	main(int ac, char **av, char **envp)
 {
 	int	*fd;
 
+	if (!envp[0])
+		return (1);
 	fd = malloc(2 * sizeof(int));
 	if (ac == 5)
 	{
 		fd[0] = open(av[1], O_RDONLY);
 		if (fd[0] < 0)
+		{
 			perror("");
+			ft_printf("%s\n", av[1]);
+		}
 		fd[1] = open(av[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
 		if (fd[1] < 0)
+		{
 			perror("");
-		if (fd[0] < 0 || fd[1] < 0)
+			ft_printf("%s\n", av[2]);
+		}
+		if (fd[1] < 0)
+		{
+			free (fd);
 			return (1);
-		pipex(fd, av, envp);
+		}
+		//if we cant open the first file then we still need to proceed with the second
+		if (pipex(fd, av, envp) != 0)
+		{
+			free(fd);
+			return (2);
+		}
 		free (fd);
 		return (0);
 	}
@@ -47,32 +66,93 @@ int	main(int ac, char **av, char **envp)
 
 int	pipex(int *fd, char **av, char **envp)
 {
-	pid_t	pid1;
+	pid_t	pid[2];
 	int		*end;
 	char	**paths;
+	int		status;
 
+	pid[0] = -42;
+	pid[1] = 42;
 	end = malloc(2 * sizeof(int));
 	if (pipe(end) == -1)
+	{
+		free (end);
 		return (1);
+	}
 	paths = find_path(envp);
 	if (!paths)
+	{
+		free (end);
 		return (1);
-	pid1 = fork();
-	if (pid1 < 0)
+	}
+	if (fd[0] >= 0)
+	{
+		//ft_printf("ENTERED A CHILD 1\n");
+		pid[0] = fork();
+		if (pid < 0)
+		{
+			free_arr_str(paths);
+			free (end);
+			return (1);
+		}
+		if (pid[0] == 0)
+		{
+			if (child_one(fd, av[2], paths, end) != 0)
+			{
+				perror("");
+				free_arr_str(paths);
+				free (end);
+				return (1);
+			}
+		}
+	}
+	if (pid[0] != 0)
+		pid[1] = fork();
+	if (pid[1] < 0)
+	{
+		free_arr_str(paths);
+		free (end);
 		return (1);
-	if (pid1 == 0)
-		if (child_one(fd, av[2], paths, end) < 0)
+	}
+	if (pid[1] == 0)
+	{
+		//ft_printf("ENTERED A CHILD 2\n");
+		if (child_two(fd, av[3], paths, end) != 0)
+		{
+			perror("");
+			free_arr_str(paths);
+			free (end);
 			return (1);
-	// what happens if execve fails? should it fork still or not?
-	pid1 = fork();
-	if (pid1 == 0)
-		if (child_two(fd, av[3], paths, end) < 0)
-			return (1);
-	if (close(fd[0]) < 0 || close (fd[1]) < 0 || \
+		}
+	}
+	close (fd[0]);
+	if (close (fd[1]) < 0 || \
 	close(end[0] < 0 || close(end[1]) < 0))
+	{
+		free_arr_str(paths);
+		free (end);
 		return (1);
+	}
+	close (end[0]);
 	free_arr_str(paths);
 	free (end);
+	if (fd[0] >= 0)
+		waitpid(pid[0], NULL, 0);
+	waitpid(pid[1], &status, 0);
+	// if (close (fd[0]) < 0)
+	// 	ft_printf("fd[0] got closed again\n");
+	// if (close (fd[0]) < 0)
+	// 	ft_printf("fd[1] got closed again\n");
+	// if (close (end[0]) < 0)
+	// 	ft_printf("end[0 got closed again\n");
+	// if (close (end[1]) < 0)
+	// 	ft_printf("end[1] got closed again\n");
+	// while (1)
+	// {
+	// 	;
+	// }
+	if (!WIFEXITED(status) || WEXITSTATUS(status) != 0)
+		return (2);
 	return (0);
 }
 
@@ -84,8 +164,8 @@ int	child_two(int *fd, char *arg_cmd_2, char **paths, int *end)
 
 	dup2(end[0], STDIN_FILENO);
 	dup2(fd[1], STDOUT_FILENO);
-	if (close(end[0]) < 0 || close(end[1]) < 0 || \
-	close(fd[0]) < 0 || close (fd[1]) < 0)
+	close(fd[0]);
+	if (close(end[0]) < 0 || close(end[1]) < 0 || close (fd[1]) < 0)
 		return (1);
 	args = ppx_split(arg_cmd_2, ' ');
 	i = 0;
@@ -96,7 +176,6 @@ int	child_two(int *fd, char *arg_cmd_2, char **paths, int *end)
 		free (cmd);
 		i ++;
 	}
-	free_arr_str(paths);
 	free_arr_str(args);
 	return (1);
 }
@@ -108,11 +187,10 @@ int	child_one(int *fd, char *arg_cmd_1, char **paths, int *end)
 	int		i;
 
 	dup2(fd[0], STDIN_FILENO);
-	dup2(end[1], STDOUT_FILENO); //now the stdin is fd[0], and stdout will be routed into the end[1] and then we read from end[0];
-	if (close(end[0]) < 0 || close(fd[0]) < 0 || close (fd[1]) < 0 || close(end[1]))
+	dup2(end[1], STDOUT_FILENO);
+	if (close(end[0]) < 0 || close(fd[0]) < 0 || \
+	close (fd[1]) < 0 || close(end[1]))
 		return (1);
-	// !!!!!! NOT AN ABSOLUTE PATH (but shell behaves in the same way) !!!!!!!!
-	// so when < ./testfilesshell/infile ls -l > ./testfilesshell/outfile it outpputs for the current folder
 	args = ppx_split(arg_cmd_1, ' ');
 	i = 0;
 	while (paths[i])
@@ -122,8 +200,6 @@ int	child_one(int *fd, char *arg_cmd_1, char **paths, int *end)
 		free (cmd);
 		i ++;
 	}
-	//perror(""); //will be extra, already has error by zsh
-	free_arr_str(paths);
 	free_arr_str(args);
 	return (1);
 }
@@ -178,6 +254,7 @@ void	free_arr_str(char **arr)
 			free(arr[i]);
 			i ++;
 		}
-		free(arr);
+		free (arr);
 	}
+	arr = NULL;
 }
